e3a67526dc79dab515c0c3f2beac23457ff0c8c7
Juanctshub <juancts@example.com>
2026-04-02 10:26:11 -0400

    fix: capacitor instant startup and invisible splash screen for android apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.eatly.app',
   appName: 'Eatly',
   webDir: 'out',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      backgroundColor: "#030712",
+      showSpinner: false,
+      androidScaleType: "CENTER_CROP"
+    },
+  },
 };
 
 export default config;
